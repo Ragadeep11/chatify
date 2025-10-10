6d7d7c6e31281ec3d3920957f2cb7060271752c3
Ragadeep11 <ragadeep1103@gmail.com>
2025-10-10 21:43:32 +0530

    Update project files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,24 @@
 // const express=require('express')
 import express from 'express';
 import dotenv from 'dotenv'
+import path from "path"
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 const app=express()
-
+const __dirname=path.resolve()
 dotenv.config()
 const PORT=process.env.PORT
 
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+// make ready for deployment
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log("server is listening at "+PORT)
